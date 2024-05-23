622a3108ce3df7a3b6ff7f14656f9d8a1a027825
SaddamHussein919 <137503910+SaddamHussein919@users.noreply.github.com>
2024-05-23 10:08:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+function exercicio1() {
+    var nome
+    var idade
+    var salario
+    var cargo
+    var turno
+    var setor
+}
+
+exercicio1()
+
+function exercicio2() {
+    var nomeDaEscola
+    var estado
+    var numeroDeProfessores
+    var cidade
+    var numeroDeMilitares
+    var logradouro
+    var endereço
+    var numeroDeAlunos
+    var linguaPortuguesa
+    var matematica
+    var geografia
+    var historia
+}
+
+exercicio2()
+
+function exercicio3() {
+    var valor1 = 10;
+    var valor2 = 5;
+    var valor3 = "2";
+    var valor4 = 8;
+    var valor5 = -5;
+    var soma1 = valor1 + valor2;
+    console.log(soma1);
+    var soma1 = valor1 + valor2 + valor4;
+    console.log(soma1);
+    var soma1 = valor1 + valor2 - valor5;
+    console.log(soma1);
+    var soma1 = valor1 + valor3;
+    console.log(soma1);
+    var soma1 = valor1 * valor2;
+    console.log(soma1);
+    var soma1 = (valor1 * valor2) / 2;
+    console.log(soma1);
+    var soma1 = (valor1 + valor2 + valor4 + valor5) / 4;
+    console.log(soma1);
+}
+
+exercicio3()
+
+function exercicio4() {
+    var v1 = 2;
+    var v2 = 3;
+    var v3 = 5;
+    var v4 = 6;
+    var somaDosValores = v1 + v2 + v3 + v4;
+    console.log(somaDosValores);
+    var mediaDosValores = somaDosValores / 4;
+    console.log(mediaDosValores);
+}
+
+exercicio4()
+
+function exercicio5() {
+    var nota1 = 64;
+    var nota2 = 45;
+    var nota3 = 60;
+    var somaDasNotas = nota1 + nota2 + nota3;
+    var media = somaDasNotas / 3;
+    if (media >= 60) {
+        console.log("Aluno aprovado");
+    } else {
+        console.log("Aluno reprovado");
+    }
+}
+
+exercicio5()
+
+function exercicio6() {
+    mediaAluno = 60;
+    alunoEmCurso = true;
+    if (mediaAluno >= 60, alunoEmCurso = true) {
+        console.log("Aluno aprovado com certificado");
+    } else if (mediaAluno >= 60, alunoEmCurso = false ) {
+        console.log("Aluno aprovado");
+    } else {
+        console.log("Aluno reprovado");
+    }
+}
+
+exercicio6()
